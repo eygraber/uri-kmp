2ac5f911f883f8d4939e128d22add8852bc5b32f
Eliezer Graber <eygraber@gmail.com>
2022-04-24 03:07:40 -0400

    Fix some issues with the GitHub release workflow
      - Don't close and release repository when building artifacts
      - Try to fix Windows expansion issue

--- release_windows.bat
+++ release_windows.bat
@@ -1,1 +1,3 @@
-gradlew :uri:clean :uri:build :uri:publishMingwX64PublicationToMavenCentralRepository :uri:publishMingwX86PublicationToMavenCentralRepository %*% --no-daemon --no-parallel
+@echo off
+SETLOCAL EnableDelayedExpansion
+gradlew :uri:clean :uri:build :uri:publishMingwX64PublicationToMavenCentralRepository :uri:publishMingwX86PublicationToMavenCentralRepository !*! --no-daemon --no-parallel
