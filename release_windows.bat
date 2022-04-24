c454bacedaed3a1840fcf27e5a2f58660cfab78b
Eliezer Graber <eygraber@gmail.com>
2022-04-24 03:30:16 -0400

    Some more fixes for GitHub release workflow

--- release_windows.bat
+++ release_windows.bat
@@ -1,3 +1,3 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
-gradlew :uri:clean :uri:build :uri:publishMingwX64PublicationToMavenCentralRepository :uri:publishMingwX86PublicationToMavenCentralRepository !*! --no-daemon --no-parallel
+gradlew :uri:publishMingwX64PublicationToMavenCentralRepository :uri:publishMingwX86PublicationToMavenCentralRepository !*! --no-daemon --no-parallel
