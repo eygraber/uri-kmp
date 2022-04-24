b6e89f3e3d850398052963d92226d8634c20f2fb
Eliezer Graber <eygraber@gmail.com>
2022-04-24 03:39:56 -0400

    Don't skip publication tasks based on OS

--- build-logic/src/main/kotlin/publish.gradle.kts
+++ build-logic/src/main/kotlin/publish.gradle.kts
@@ -1,7 +1,4 @@
 import com.vanniktech.maven.publish.SonatypeHost
-import org.apache.tools.ant.taskdefs.condition.Os
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
 
 plugins {
   id("org.jetbrains.dokka")
@@ -10,28 +7,4 @@ plugins {
 
 mavenPublish {
   sonatypeHost = SonatypeHost.S01
-}
-
-plugins.withType<KotlinBasePluginWrapper> {
-  with(extensions.getByType<KotlinMultiplatformExtension>()) {
-    val publicationsFromWindows = listOf("mingwX64", "mingwX86")
-
-    val publicationsFromMacos =
-      targets.names.filter {
-        it.startsWith("macos") || it.startsWith("ios") || it.startsWith("watchos") || it.startsWith("tvos")
-      }
-
-    val publicationsFromLinux = publishing.publications.names - publicationsFromWindows - publicationsFromMacos
-
-    val publicationsFromThisPlatform = when {
-      Os.isFamily(Os.FAMILY_WINDOWS) -> publicationsFromWindows
-      Os.isFamily(Os.FAMILY_MAC) -> publicationsFromMacos
-      Os.isFamily(Os.FAMILY_UNIX) -> publicationsFromLinux
-      else -> error("Expected Windows, Mac, or Linux host")
-    }
-
-    tasks.withType(AbstractPublishToMaven::class).all {
-      onlyIf { publication.name in publicationsFromThisPlatform }
-    }
-  }
 }
