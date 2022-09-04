618544e7628939eb64bf041fb8e06e5a1da3efb9
Eliezer Graber <me@eygraber.com>
2022-09-04 19:01:40 -0400

    Some updates to the github workflows

--- release_windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-SETLOCAL EnableDelayedExpansion
-gradlew :uri:publishMingwX64PublicationToMavenCentralRepository :uri:publishMingwX86PublicationToMavenCentralRepository !*! --no-daemon --no-parallel
