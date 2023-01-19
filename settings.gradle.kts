1f7f1684cca61d044b5a0f0d53e5b41ceac02cfc
Eliezer Graber <me@eygraber.com>
2023-01-18 21:17:31 -0500

    Migrate to gradle-conventions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+import com.eygraber.conventions.Env
+import com.eygraber.conventions.repositories.addCommonRepositories
+
 pluginManagement {
   repositories {
     google {
@@ -7,63 +10,63 @@ pluginManagement {
         includeGroupByRegex("androidx.*")
       }
     }
+
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") {
+      content {
+        includeGroupByRegex("org\\.jetbrains.*")
+      }
+    }
+
     mavenCentral()
-    gradlePluginPortal()
+
     maven("https://oss.sonatype.org/content/repositories/snapshots") {
       mavenContent {
         snapshotsOnly()
       }
     }
+
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots") {
       mavenContent {
         snapshotsOnly()
       }
     }
-  }
 
-  @Suppress("UnstableApiUsage")
-  includeBuild("build-logic")
+    gradlePluginPortal()
+  }
 }
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
+  // comment this out for now because it doesn't work with KMP js
+  // https://youtrack.jetbrains.com/issue/KT-51379/
+  // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
   repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("androidx.*")
-      }
-    }
-
-    mavenCentral()
-
-    maven("https://oss.sonatype.org/content/repositories/snapshots") {
-      mavenContent {
-        snapshotsOnly()
-      }
-    }
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots") {
-      mavenContent {
-        snapshotsOnly()
-      }
-    }
+    addCommonRepositories(
+      includeMavenCentral = true,
+      includeMavenCentralSnapshots = true,
+      includeGoogle = true,
+      includeJetbrainsCompose = true
+    )
   }
 }
 
 plugins {
+  id("com.eygraber.conventions.settings") version "0.0.29"
   id("com.gradle.enterprise") version "3.12.2"
   id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
 }
 
 rootProject.name = "uri-kmp"
 
+include(":uri")
+
 gradleEnterprise {
   buildScan {
     termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-    publishAlways()
+    if(Env.isCI) {
+      termsOfServiceAgree = "yes"
+      publishAlways()
+    }
   }
 }
-
-include(":uri")
