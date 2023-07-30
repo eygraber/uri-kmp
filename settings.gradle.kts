bce6ed81d33273ef73379ff31b65272726ff2856
Eliezer Graber <me@eygraber.com>
2023-07-30 18:13:53 -0400

    Update conventions to 0.0.44

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -52,9 +52,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-  id("com.eygraber.conventions.settings") version "0.0.39"
+  id("com.eygraber.conventions.settings") version "0.0.44"
   id("com.gradle.enterprise") version "3.14.1"
-  id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
 }
 
 rootProject.name = "uri-kmp"
